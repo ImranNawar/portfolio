cb1255e166b82c0703bcc1a2159decf87482184e
ImranNawar <imran1nawar@gmail.com>
2023-06-30 14:48:16 +0500

    Resume

--- src/components/pages/homepage-comps/MobileSocialLinks.jsx
+++ src/components/pages/homepage-comps/MobileSocialLinks.jsx
@@ -41,7 +41,7 @@ const MobileSocialLinks = () => {
                     <BsFillPersonLinesFill size={20} /> Resume 
                 </>
             ),
-            href: '/FrontendDeveloper-DiegoTellez.pdf',
+            href: '/Imran_Resume.pdf',
             style: 'rounded-br-md',
             download: true
         }
